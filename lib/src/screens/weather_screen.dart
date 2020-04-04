2af68904f533323acfdc9e7c27ef26cbe3c45918
jeanffc <jeanffc@gmail.com>
2020-04-04 17:42:29 -0300

    added today's time

--- lib/src/screens/weather_screen.dart
+++ lib/src/screens/weather_screen.dart
@@ -1,3 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class WeatherScreen extends StatefulWidget {}
+class WeatherScreen extends StatefulWidget {
+  @override
+  _WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                DateFormat('E, MMMM d yy HH:mm').format(DateTime.now()),
+                style:
+                    TextStyle(color: Colors.white.withAlpha(80), fontSize: 14),
+              )
+            ],
+          ),
+        ),
+        body: Material(
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(color: Colors.black),
+          ),
+        ));
+  }
+}
