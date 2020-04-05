b4e22392fed00f4d37e032db177da048c0bcbbc4
jeanffc <jeanffc@gmail.com>
2020-04-05 10:37:12 -0300

    added weather model

---
+++ lib/src/model/weather.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app2/src/utils/converters.dart';
+
+class Weather {
+  int id;
+  int time;
+  int sunrise;
+  int sunset;
+  int humidity;
+
+  String description;
+  String iconCode;
+  String main;
+  String cityName;
+
+  double windSpeed;
+
+  Temperature temperature;
+  Temperature maxTemperature;
+  Temperature minTemperature;
+
+  List<Weather> forecast;
+
+  Weather(
+      {this.id,
+      this.time,
+      this.sunrise,
+      this.sunset,
+      this.humidity,
+      this.description,
+      this.iconCode,
+      this.main,
+      this.cityName,
+      this.windSpeed,
+      this.temperature,
+      this.maxTemperature,
+      this.minTemperature,
+      this.forecast});
+
+  static Weather fromJson(Map<String, dynamic> json) {
+    final weather = json['weather'][0];
+    return Weather(
+      id: weather['id'],
+      time: json['dt'],
+      description: weather['description'],
+      iconCode: weather['icon'],
+      main: weather['main'],
+      cityName: json['name'],
+      temperature: Temperature(intToDouble(json['main']['temp'])),
+      maxTemperature: Temperature(intToDouble(json['main']['temp_max'])),
+      minTemperature: Temperature(intToDouble(json['main']['temp_min'])),
+      sunrise: json['sys']['sunrise'],
+      sunset: json['sys']['sunset'],
+      humidity: json['main']['humidity'],
+      windSpeed: intToDouble(json['wind']['speed']),
+    );
+  }
+
+  static List<Weather> fromForecastJson(Map<String, dynamic> json) {
+    final weathers = List<Weather>();
+    for (final item in json['list']) {
+      weathers.add(Weather(
+          time: item['dt'],
+          temperature: Temperature(intToDouble(
+            item['main']['temp'],
+          )),
+          iconCode: item['weather'][0]['icon']));
+    }
+    return weathers;
+  }
+}
